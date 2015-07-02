f0e0c9f4aeec1583aadee9586035170f3b295ea3
Jamie Binns <jamie.m.binns@gmail.com>
2015-07-02 14:12:29 -0600

    First commit

---
+++ main.js
@@ -0,0 +1,43 @@
+
+// Problem 1 //
+
+// var firstReverse = function(string){
+// 	var newString = string.split('').reverse();
+// 	return newString.join('')
+
+// }
+
+// console.log(firstReverse('Jamie'))
+
+
+
+// Problem 2 //
+
+// var swapCase = function(string){
+// 	var returnArray = [];
+// 	var splitArray = string.split('').forEach(function(element){
+// 		if(element === element.toUpperCase()){
+// 			returnArray.push(element.toLowerCase());
+// 		}
+// 		else{
+// 			returnArray.push(element.toUpperCase());
+// 		}
+// 	});
+// 	return returnArray.join('');
+// }
+
+// console.log(swapCase('Francesca Blume'))
+
+
+
+// Problem 3 //
+
+var letterCount = function(string){
+	var stringArray = string.split(' ');
+	stringArray.forEach(function(word){
+		word
+	})
+
+}
+
+letterCount('Today is the greatest day ever!')
